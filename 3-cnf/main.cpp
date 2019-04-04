43b7abb96ffee9601d985fb609c3bc109a23a6ae
khlebous <khlebousa@gmail.com>
2019-04-04 19:53:43 +0200

    return if not two args

--- 3-cnf/main.cpp
+++ 3-cnf/main.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		cout << "Wrong arg count, should be path to .txt file" << endl;
+		return 0;
+	}
+
 	cout << "Hello 3-cnf!" << endl;
 	return 0;
 }
