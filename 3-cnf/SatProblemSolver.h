8fbc1441d5562a77b0c76d8000ff566099f8610e
McThrok <wojtek.trze@gmail.com>
2019-06-02 11:28:29 +0100

    cleanup

--- 3-cnf/SatProblemSolver.h
+++ 3-cnf/SatProblemSolver.h
@@ -16,47 +16,33 @@ private:
 
 	bool Solve3SnfRec(Formula const& formula, map<int, bool> & literalsMap)
 	{
-		//cout << "1. TRY SOLVE FORMULA: " << formula.ToString() << endl;
 		if (formula.ClausesCount() == 0)
-		{
-			//cout << "RETURN FALSE" << endl;
 			return false;
-		}
 
 		Clause clause = formula[0];
 
-		int c[3];
-		for (size_t i = 0; i < clause.Size(); i++)
-			c[i] = clause[i];
+
 
 		Formula f_x;
-		//cout << "2. Substitute " << to_string(clause[0]) << " as true to formula ";
-		//cout << formula.ToString() << endl;
 		InsertValueToMap(literalsMap, clause[0]);
-		//cout << "Current map: ";
-		//for (auto& x : literalsMap) //cout << x.first << "-" << boolalpha << x.second << " ";
-		//cout << endl;
 
 		State state = formula.SubstituteTrue(clause[0], f_x);
 		if (state == State::ALWAYS)
 			return true;
 
-		//cout << "3. Result: " << f_x.ToString() << endl;
-		//cout << endl;
-		//cout << "4. Try solve formula f_x: " << f_x.ToString() << endl;
-		//cout << endl;
 		if (state == State::UNKNOWN)
 			if (Solve3SnfRec(f_x, literalsMap))
 				return true;
 
 		literalsMap.erase(clause[0]);
+
 		if (clause.Size() == 1)
 			return false;
 
-		//cout << "NOT OK, lets try -x" << endl;
+
+
 		Formula f_negx;
 		InsertValueToMap(literalsMap, -clause[0]);
-
 
 		state = formula.SubstituteTrue(-clause[0], f_negx);
 
@@ -68,27 +54,12 @@ private:
 			return false;
 		}
 
-		//cout << "5. Substitute " << to_string(-clause[0]) << " as true to formula ";
-		//cout << formula.ToString() << endl;
-		//cout << "6. Result: " << f_negx.ToString() << endl;
-
 		Formula f_negx_y;
 		InsertValueToMap(literalsMap, clause[1]);
-		//cout << "Current map: ";
-		//for (auto& x : literalsMap) //cout << x.first << "-" << boolalpha << x.second << " ";
-		//cout << endl;
-		//cout << "7. Substitute " << to_string(clause[1]) << " as true to formula ";
-		//cout << f_negx.ToString() << endl;
 
 		state = f_negx.SubstituteTrue(clause[1], f_negx_y);
 		if (state == State::ALWAYS)
 			return true;
-
-
-		//cout << "8. Result: " << f_negx_y.ToString() << endl;
-
-		//cout << "9. Try solve formula f_negx_y: " << f_negx_y.ToString() << endl;
-		//cout << endl;
 
 		if (state == State::UNKNOWN)
 			if (Solve3SnfRec(f_negx_y, literalsMap))
@@ -99,10 +70,7 @@ private:
 		if (clause.Size() == 2)
 			return false;
 
-		//cout << "NOT OK, lets try -x and -y" << endl;
 
-		//cout << "10. Substitute " << to_string(-clause[1]) << " as true to formula ";
-		//cout << f_negx.ToString() << endl;
 
 		Formula f_negx_neg_y;
 		InsertValueToMap(literalsMap, -clause[1]);
@@ -115,18 +83,9 @@ private:
 			literalsMap.erase(-clause[1]);
 			return false;
 		}
-		//cout << "11. Result: " << f_negx_neg_y.ToString();
 
 		Formula f_negx_neg_y_z;
 		InsertValueToMap(literalsMap, clause[2]);
-		//InsertValueToMap(literalsMap, clause[1]);
-
-		//cout << "Current map: ";
-		//for (auto& x : literalsMap) //cout << x.first << "-" << boolalpha << x.second << " ";
-		//cout << endl;
-		//cout << "12. Substitute " << to_string(clause[2]) << " as true to formula ";
-		//cout << f_negx_neg_y_z.ToString() << endl;
-
 
 		state = f_negx_neg_y.SubstituteTrue(clause[2], f_negx_neg_y_z);
 
@@ -137,9 +96,6 @@ private:
 			literalsMap.erase(-clause[2]);
 			return false;
 		}
-
-		//cout << "13. Result: " << f_negx_neg_y_z.ToString();
-		//cout << "14. Try solve formula f_negx_neg_y_z: " << f_negx_neg_y_z.ToString() << endl;
 
 		return (Solve3SnfRec(f_negx_neg_y_z, literalsMap));
 	}
--- 3-cnf/main.cpp
+++ 3-cnf/main.cpp
@@ -7,58 +7,15 @@
 #include "SatProblemSolver.h"
 using namespace std;
 
-bool GetClauseValue(Clause c, map<int, bool> map)
-{
-	for (size_t i = 0; i < c.Size(); i++)
-	{
-		int iteral = c[i];
-		if (iteral >= 0 && map[iteral] || iteral < 0 && !map[-iteral])
-			return true;
-	}
-
-	return false;
-}
-
-bool GetFormulaValue(Formula f, map<int, bool> map)
-{
-	for (size_t i = 0; i < f.ClausesCount(); i++)
-		if (!GetClauseValue(f[i], map))
-			return false;
-
-	return true;
-}
-
 int main(int argc, char* argv[])
 {
-	/*if (argc != 3)
+	if (argc != 3)
 	{
 		cout << "Wrong args count." << endl;
 		return 0;
-	}*/
+	}
 
-	vector<string> data;// = TxtReader::ReadFile(argv[1]);
-
-
-	data.push_back("-3 2 -4");
-	data.push_back("-5 4 2");
-	data.push_back("-1 1 -4");
-	data.push_back("4 -4 -4");
-	data.push_back("-4 2 3");
-	data.push_back("-2 5 -3");
-	data.push_back("-2 -4 3");
-	data.push_back("-4 5 5");
-	data.push_back("-5 -1 -4");
-	data.push_back("3 -4 -2");
-	data.push_back("-4 -5 -1");
-	data.push_back("4 1 1");
-	data.push_back("5 4 1");
-	data.push_back("-3 -1 1");
-	data.push_back("3 -2 -1");
-	data.push_back("-2 -4 -5");
-	data.push_back("4 -5 4");
-	data.push_back("1 3 -5");
-	data.push_back("-1 5 3");
-	data.push_back("5 3 -4");
+	vector<string> data = TxtReader::ReadFile(argv[1]);
 
 	Formula formula;
 	if (!Parser::TryParseStringToFormula(data, ' ', formula))
@@ -76,10 +33,8 @@ int main(int argc, char* argv[])
 	unordered_set<int> formulaLiterals = formula.GetLiterals();
 	bool isSat = sps.Solve3Snf(formula, literalsMap);
 
-	bool a = GetFormulaValue(formula, literalsMap);
-
 	stringstream outputData = Parser::ParseToStringStream(isSat, formulaLiterals, literalsMap);
-	//TxtWriter::WriteToFile(argv[2], outputData);
+	TxtWriter::WriteToFile(argv[2], outputData);
 	cout << outputData.str();
 
 	return 0;
